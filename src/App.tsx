8f6c99882b6cecd25e71a55ec283b2b57252dde8
Yash-scaletech <yash.upadhyay@scaletech.xyz>
2023-07-04 19:24:21 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 import 'assets/styles/app.scss';
 
 import Layout from 'hoc/layout/layout';
+import Dashboard from 'features/dashboard/component/dashboard';
 
 const App: React.FC = () => {
-	return <Layout />;
+	return (
+		<Layout>
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+			</Routes>
+		</Layout>
+	);
 };
 
 export default App;
